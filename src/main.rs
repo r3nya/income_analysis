a6f0e32a804b4e32175037ea0d4d69502e19c64d
Andrew Makarov <me@r3nya.ru>
2024-07-23 23:28:22 +0200

    DRY

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod output;
 
 use analyzers::{analyze_overall_data, analyze_year_data};
 use data_loader::{df_to_hashmap, get_available_years, load_data_from_csv};
-use output::{print_overall_analysis, print_year_analysis};
+use output::print_analysis;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -26,22 +26,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         Some(year) => {
             if available_years.contains(&year) {
                 let analysis = analyze_year_data(year, &income_data)?;
-                print_year_analysis(&analysis);
+                print_analysis(&analysis);
             } else {
                 println!("No data available for the year {}", year);
                 println!("Available years: {:?}", available_years);
             }
         }
         None => {
-            // Analyze and print data for each available year
             for &year in &available_years {
                 let analysis = analyze_year_data(year, &income_data)?;
-                print_year_analysis(&analysis);
+                print_analysis(&analysis);
             }
 
-            // Analyze and print overall data
             let overall_analysis = analyze_overall_data(&income_data)?;
-            print_overall_analysis(&overall_analysis);
+            print_analysis(&overall_analysis);
         }
     }
 
--- src/output.rs
+++ src/output.rs
@@ -1,39 +1,32 @@
 use crate::analyzers::YearAnalysis;
 
-pub fn print_year_analysis(analysis: &YearAnalysis) {
-    println!("\nIncome Data for {}:", analysis.year);
-    println!("{}", analysis.data_frame);
-    println!(
-        "Total Income for {}: ${:.2}",
-        analysis.year, analysis.total_income
-    );
-    println!(
-        "Average Income for {}: ${:.2}",
-        analysis.year, analysis.avg_income
-    );
-    println!(
-        "Highest Income for {}: ${:.2}",
-        analysis.year, analysis.max_income
-    );
-    println!(
-        "Lowest Income for {}: ${:.2}",
-        analysis.year, analysis.min_income
-    );
-    println!(
-        "24% of Total Income for {}: ${:.2}",
-        analysis.year, analysis.twenty_four_percent
-    );
-}
+pub fn print_analysis(analysis: &YearAnalysis) {
+    let time_period = if analysis.year == 0 {
+        "Overall".to_string()
+    } else {
+        analysis.year.to_string()
+    };
 
-pub fn print_overall_analysis(analysis: &YearAnalysis) {
-    println!("\nOverall Income Data:");
+    println!("\nIncome Data for {}:", time_period);
     println!("{}", analysis.data_frame);
-    println!("Total Income (Overall): ${:.2}", analysis.total_income);
-    println!("Average Income (Overall): ${:.2}", analysis.avg_income);
-    println!("Highest Income (Overall): ${:.2}", analysis.max_income);
-    println!("Lowest Income (Overall): ${:.2}", analysis.min_income);
     println!(
-        "24% of Total Income (Overall): ${:.2}",
-        analysis.twenty_four_percent
+        "Total Income ({}): ${:.2}",
+        time_period, analysis.total_income
+    );
+    println!(
+        "Average Income ({}): ${:.2}",
+        time_period, analysis.avg_income
+    );
+    println!(
+        "Highest Income ({}): ${:.2}",
+        time_period, analysis.max_income
+    );
+    println!(
+        "Lowest Income ({}): ${:.2}",
+        time_period, analysis.min_income
+    );
+    println!(
+        "24% of Total Income ({}): ${:.2}",
+        time_period, analysis.twenty_four_percent
     );
 }
